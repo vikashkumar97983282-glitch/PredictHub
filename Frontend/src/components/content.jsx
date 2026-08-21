bd9bb8a4b4204ae59c0253736d65bc3816d71063
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-08-22 01:04:09 +0530

    add content page for write content.

---
+++ Frontend/src/components/content.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+
+
+function Content(){
+
+
+    return(
+        <div>
+            {/* Main Content */}
+            <section className="flex-1 overflow-auto p-1">
+
+                <div className="min-h-full rounded-lg border  border-[#344154] bg-[#f8fafc] p-2">
+
+                    {/* Your dashboard content goes here */}
+                    <h1>this is PredictHub application</h1>
+                    <p>this application is use for prediction </p>
+
+                </div>
+
+            </section>
+        </div>
+    )
+}
+
+
+export default Content;
