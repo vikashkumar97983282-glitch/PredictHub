bec113b9de8c8a5d95d9f14fcc5158ea13a5d15e
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-08-21 18:51:11 +0530

    this is Backend folder using python.

---
+++ Backend/app.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="PredictHub API",
+    description="Machine Learning Prediction API",
+    version="1.0.0"
+)
+
+
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:5173"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+
+
+@app.get('/')
+def home():
+    return {'message':'this is PredictHub application'}
