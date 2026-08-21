600c39ab731627b533d4224565aeb55d4f178f95
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-08-21 19:22:01 +0530

    add cors origin .

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
@@ -9,16 +10,19 @@ app = FastAPI(
 
 
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:5173"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
 @app.get('/')
 def home():
-    return {'message':'this is PredictHub application'}
+    return {
+        'message':'this is PredictHub application from Fastapi!',
+
+        }
