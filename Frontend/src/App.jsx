121ea290da8f81af16f99998e1a9ff42e2e57c10
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-08-24 13:35:02 +0530

    add routes .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,12 +4,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home";
 import About from "./pages/about";
+import Analytics from "./pages/analytics";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/analytics" element={<Analytics />} />
     </Routes>
   );
 }
