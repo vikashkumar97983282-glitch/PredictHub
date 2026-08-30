98b4053a51ff8cd9b36eb745a882cbe2a4f5420c
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-08-31 01:17:42 +0530

    fix vercel build

--- Frontend/src/pages/about.jsx
+++ Frontend/src/pages/about.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Sidebar from "../components/sidebar";
 import Navbar from "../components/header";
-import Footer from "../components/Footer";
+import Footer from "../components/footer";
 import { useState } from "react";
 
 function About() {
