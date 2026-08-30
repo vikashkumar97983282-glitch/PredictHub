421f40dd8b56d66e002b94ff1b6e8b8001d72c56
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-08-31 01:35:32 +0530

    allow origin for predicthub frontend

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -12,7 +12,10 @@ def create_app() -> FastAPI:
     ### Cors middleware
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173"],
+        allow_origins=[
+            "http://localhost:5173",
+            "https://predicthub-lac.vercel.app"
+            ],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
