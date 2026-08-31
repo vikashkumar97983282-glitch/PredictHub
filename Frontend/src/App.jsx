011d704f19d7df298864f6dda6ddcd793f24b3df
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-08-31 15:27:24 +0530

    add coming soon route .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Prediction from "./pages/prediction";
+import ComingSoon from "./pages/coming-soon";
 import PlacementForm from "./pages/placement";
 import Analytics from "./pages/analytics";
 import Trending from "./pages/trending";
@@ -20,6 +21,7 @@ function App() {
       <Route path="/prediction">
         <Route index element={<Prediction />} />
         <Route path="placement" element={<PlacementForm />} />
+        <Route path="*" element={<ComingSoon />} />
       </Route>
 
       <Route path="/analytics" element={<Analytics />} />
