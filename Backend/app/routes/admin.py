44cec18b43e53236342e27a9b2265c0dd8acedd3
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-09-02 15:38:13 +0530

    add admin login and create routes.

--- Backend/app/routes/admin.py
+++ Backend/app/routes/admin.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from app.schemas.admin_schema import AdminLogin, AdminLoginResponse
+from app.schemas.admin_schema import AdminLogin, AdminLoginResponse, AdminCreate
+from app.database.db_connection import db,collection
 
 
 
@@ -16,6 +17,37 @@ def home():
         'message':'this is PredictHub application from Fastapi! from admin router',
 
         }
+
+
+
+@router.post('/create_admin')
+async def create_admin(data: AdminCreate):
+    if data.password != data.confirm_password:
+        return {
+            'message':'Password and confirm password do not match'
+        }
+
+    user = await collection.count_documents({})
+
+    if user > 0:
+        return {
+            'message':'Admin already exists'
+        }
+    
+    admin = {
+        "name": data.name,
+        "email": data.email.lower(),
+        "password": data.password,
+        "role": "admin",
+    }
+
+    result = await db.admin.insert_one(admin)
+
+    return {
+        "message": "Admin created successfully",
+        "user_id": str(result.inserted_id),
+        "role": "admin",
+    }
 
 
 
