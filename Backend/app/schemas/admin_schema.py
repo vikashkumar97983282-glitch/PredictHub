03d02392348c67596adec8c58cfacb34efa48830
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-09-02 15:36:36 +0530

    create admin login, create and response schemas

--- Backend/app/schemas/admin_schema.py
+++ Backend/app/schemas/admin_schema.py
@@ -2,6 +2,13 @@ from pydantic import BaseModel, Field , EmailStr
 
 
 
+class AdminCreate(BaseModel):
+    name: str = Field(..., description="Admin name")
+    age: int = Field(..., description="Admin age")
+    email: EmailStr = Field(..., description="Admin email address")
+    password: str = Field(..., description="Admin password")
+    confirm_password: str = Field(..., description="Confirm admin password")
+
 class AdminLogin(BaseModel):
     email: EmailStr = Field(..., description="Admin email address")
     password: str = Field(..., description="Admin password")
