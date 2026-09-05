fc720c2a23454538b4de8b2e41314814edf36b18
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-09-05 21:38:22 +0530

    update model response

--- Backend/app/models/placement_model.py
+++ Backend/app/models/placement_model.py
@@ -15,4 +15,4 @@ def placement_prediction(data):
             "resume_score":data.resume_score}
     ])
     prediction = model.predict(features)[0]
-    return prediction
+    return prediction*100
