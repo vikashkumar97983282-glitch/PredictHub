374e6bfb32f725093cefc9738d8878baeb3d87ff
vikashkumar97983282-glitch <vikashkumar97983282@gmail.com>
2026-09-07 16:33:28 +0530

    replace route and fix error .

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,7 +9,7 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Prediction from "./pages/prediction";
 import ComingSoon from "./pages/coming-soon";
-import PlacementForm from "./pages/placement";
+import Form from "./pages/prediction_form";
 import Analytics from "./pages/analytics";
 import Trending from "./pages/trending";
 import Community from "./pages/community";
@@ -104,22 +104,16 @@ function App() {
             element={<Prediction />}
           />
 
-          {/* /prediction/placement */}
+          {/* /prediction/coming-soon/stock-price */}
           <Route
-            path="placement"
-            element={<PlacementForm />}
-          />
-
-          {/* /prediction/stock-price */}
-          <Route
-            path="stock-price"
+            path="coming-soon/*"
             element={<ComingSoon />}
           />
 
-          {/* /prediction/data-analysis */}
+          {/* Dynamic model form, for example /prediction/placement */}
           <Route
-            path="data-analysis"
-            element={<ComingSoon />}
+            path=":modelKey"
+            element={<Form />}
           />
 
           {/* Unknown prediction pages */}
